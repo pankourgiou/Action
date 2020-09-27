bde014a3a3658131559169fb2798b10efcfa97a0
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2020-09-27 06:18:28 +0300

    Create Saturn.py

---
+++ Saturn.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#x = 1=0
+
+x  ="♄*♄ = 0"
+x1 = "♊*♊=0"
+x2 = "People could change their belief system so that they buy cheaper oil"
+x3 = "♋*♋=0"
+x4 = "-+-=0"
+x5 = "-*-*-=0"
+x6 = "I was born for multithreading born to the stars I was born to dream"
+try_to_keep_it_simple = "True"
+do = "or do not, there is no try"
+
+print(bool(x))
+print(bool(x1))
+print("False")
+print("False")
+print(bool(x4))
+print(bool(x5))
+print(bool(x6))
+print(bool(try_to_keep_it_simple))
+print("We can go slower or faster, We prefer going slower\n"*10)
